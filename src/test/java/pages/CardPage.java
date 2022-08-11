e15c920cae72b81c31a6d6efcff25ddbc44c389f
AnsellaAli <99746028+AnsellaAli@users.noreply.github.com>
2022-08-11 11:33:59 +0300

    Update CardPage.java

--- src/test/java/pages/CardPage.java
+++ src/test/java/pages/CardPage.java
@@ -4,20 +4,24 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$;
 import static org.junit.Assert.assertTrue;
 
 public class CardPage {
 
-    private final By TAB_LIST = By.cssSelector(".Wrapper-sc-1vydk7-0.jLWLXd.TabText-sc-tyqhzb-3.faRXmE");
+    private final List<SelenideElement> TAB_LIST = $$(".Wrapper-sc-1vydk7-0.jLWLXd.TabText-sc-tyqhzb-3.faRXmE");
     private final By CARD_LIST = By.cssSelector(".Wrapper-sc-6nwvzq-0.kRJvZg.styled__TitleMobile-sc-m0n4zo-8.frcrJF");
 
 
     @Step("Выбрать сортировку {0}")
     public CardPage selectTab(String tabName) {
-       $$(TAB_LIST).shouldBe(sizeGreaterThan(0)).find(text(tabName)).click();
+        $$(TAB_LIST).shouldBe(sizeGreaterThan(0)).find(text(tabName)).click();
+
         return this;
     }
 
@@ -27,7 +31,7 @@ public class CardPage {
             String card = $$(CARD_LIST).get(i).getOwnText();
             card.equals(cardName);
         }
-        //$$(CARD_LIST).shouldHave(sizeGreaterThan(0)).stream().map(SelenideElement::getSelectedText).forEach(name -> assertTrue(name.contains(cardName)));
+       // $$(CARD_LIST).shouldHave(sizeGreaterThan(0)).stream().map(SelenideElement::getSelectedValue).forEach(name -> assertTrue(name.contains(cardName)));
         return this;
     }
 }
